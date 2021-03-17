0cb2d8ef8a7a2edb8c1defe500b0f16e5f021bba
joavasqu <joaquin.vasquez.elias@gmail.com>
2021-03-17 10:27:41 -0300

    se limpió todo y se dejó como era

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-alert('Hello World');
-let link1 = document.getElementById('link1');
-
-function holaMundo (){
-    link1.style.backgroundColor = 'yellow';
-    document.body.style.backgroundColor = 'green';
-}
-
-link1.onclick = holaMundo;
 
